771f7e41a2868311ba5447a35eb7e2cdbdc93bab
AleZamoraEdu <alezamora247@gmail.com>
2024-11-22 15:05:29 -0400

    JsonToHashMap update

--- Proyecto2_EDD/src/proyecto2_edd/dataStructures/hashMap/JsonToHashMap.java
+++ Proyecto2_EDD/src/proyecto2_edd/dataStructures/hashMap/JsonToHashMap.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 
 public class JsonToHashMap {
 
-    // Method for testing purposes
+    // Main method for testing purposes
     public static void main(String[] args) {
         String[] files = {"Proyecto2_EDD/Proyecto2_EDD/resources/Baratheon.json", "Proyecto2_EDD/Proyecto2_EDD/resources/Targaryen.json"};
         HashMap<String, FamilyMember> familyMap = new HashMap<>();
@@ -22,33 +22,43 @@ public class JsonToHashMap {
         try (FileReader reader = new FileReader(filePath)) {
             JSONTokener tokener = new JSONTokener(reader);
             JSONObject jsonObject = new JSONObject(tokener);
-            jsonObject.keys().forEachRemaining(house -> {
-                JSONArray members = jsonObject.getJSONArray(house);
-                for (int i = 0; i < members.length(); i++) {
-                    JSONObject member = members.getJSONObject(i);
-                    String memberName = member.keys().next();
-                    FamilyMember familyMember = new FamilyMember(memberName);
-                    JSONArray attributes = member.getJSONArray(memberName);
-                    for (int j = 0; j < attributes.length(); j++) {
-                        JSONObject attribute = attributes.getJSONObject(j);
-                        if (attribute.has("Father to")) {
-                            JSONArray children = attribute.getJSONArray("Father to");
-                            for (int k = 0; k < children.length(); k++) {
-                                familyMember.addChild(children.getString(k));
-                            }
-                        }
-                        if (attribute.has("Born to")) {
-                            familyMember.setParent(attribute.getString("Born to"));
-                        }
-                        if (attribute.has("Known throughout as")) {
-                            familyMember.setAlias(attribute.getString("Known throughout as"));
-                        }
-                    }
-                    familyMap.put(memberName, familyMember);
-                }
-            });
+            jsonObject.keys().forEachRemaining(house -> processHouse(jsonObject.getJSONArray(house), familyMap));
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    // Process each house in the JSON file
+    private static void processHouse(JSONArray members, HashMap<String, FamilyMember> familyMap) {
+        for (int i = 0; i < members.length(); i++) {
+            JSONObject member = members.getJSONObject(i);
+            String memberName = member.keys().next();
+            FamilyMember familyMember = new FamilyMember(memberName);
+            processMemberAttributes(member.getJSONArray(memberName), familyMember);
+            familyMap.put(memberName, familyMember);
+        }
+    }
+
+    // Process attributes of each family member
+    private static void processMemberAttributes(JSONArray attributes, FamilyMember familyMember) {
+        for (int j = 0; j < attributes.length(); j++) {
+            JSONObject attribute = attributes.getJSONObject(j);
+            if (attribute.has("Father to")) {
+                processChildren(attribute.getJSONArray("Father to"), familyMember);
+            }
+            if (attribute.has("Born to")) {
+                familyMember.setParent(attribute.getString("Born to"));
+            }
+            if (attribute.has("Known throughout as")) {
+                familyMember.setAlias(attribute.getString("Known throughout as"));
+            }
+        }
+    }
+
+    // Process children of a family member
+    private static void processChildren(JSONArray children, FamilyMember familyMember) {
+        for (int k = 0; k < children.length(); k++) {
+            familyMember.addChild(children.getString(k));
         }
     }
 }
